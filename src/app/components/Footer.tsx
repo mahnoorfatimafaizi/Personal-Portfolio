ce57e3a14d7d49f66c136b2313cad965947d0676
Mahnoor Fatima Faizi <mahnoorf2002@gmail.com>
2024-11-18 22:15:13 +0500

    "styling"

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,67 +1,3 @@
-// "use client";
-
-// import React, { useEffect, useState } from "react";
-// import Link from "next/link";
-
-// const Footer = () => {
-//   const [isClient, setIsClient] = useState(false);
-
-//   useEffect(() => {
-//     setIsClient(true);
-//   }, []);
-
-//   if (!isClient) {
-//     return null;
-//   }
-
-//   return (
-//     <footer className="bg-[#000000] py-16 font-serif">
-//       <div className="container mx-auto px-4 flex flex-col items-center space-y-6 md:flex-row md:justify-between md:space-y-0 mt-10">
-//         <div className="text-center md:text-left">
-//           <p className="text-white">mahnoorf2002@gmail.com</p>
-//         </div>
-
-//         <div className="flex space-x-4">
-//           <Link href="https://github.com/mahnoorfatimafaizi" passHref>
-//             <div className="text-white hover:text-purple-500 text-2xl">
-//               <i className="fab fa-github"></i>
-//             </div>
-//           </Link>
-
-//           <Link href="https://www.linkedin.com/in/mahnoor-fatima-f-76782b257/" passHref>
-//             <div className="text-white hover:text-purple-500 text-2xl">
-//               <i className="fab fa-linkedin"></i>
-//             </div>
-//           </Link>
-//         </div>
-
-//         <div className="flex flex-wrap justify-center md:justify-start space-x-6 text-white text-sm">
-//           <Link href="/" passHref>
-//             <div className="hover:underline hover:text-purple-500">Home</div>
-//           </Link>
-//           <Link href="/about" passHref>
-//             <div className="hover:underline hover:text-purple-500">About</div>
-//           </Link>
-//           <Link href="/projects" passHref>
-//             <div className="hover:underline hover:text-purple-500">
-//               Projects
-//             </div>
-//           </Link>
-//           <Link href="/contact" passHref>
-//             <div className="hover:underline hover:text-purple-500">Contact</div>
-//           </Link>
-//         </div>
-//       </div>
-
-//       <div className="container mx-auto px-4 mt-8">
-//         <hr className="border-t border-gray-300" />
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
 "use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
@@ -79,9 +15,7 @@ const Footer = () => {
 
   return (
     <footer className="bg-[#000000] py-16 font-serif">
-      <div
-        className="container mx-auto px-4 flex flex-col items-center space-y-6 md:flex-row md:justify-between md:space-y-0 mt-10 opacity-0 animate-slideInUp"
-      >
+      <div className="container mx-auto px-4 flex flex-col items-center space-y-6 md:flex-row md:justify-between md:space-y-0 mt-10 opacity-0 animate-slideInUp">
         <div className="text-center md:text-left">
           <p className="text-white">mahnoorf2002@gmail.com</p>
         </div>
@@ -93,7 +27,10 @@ const Footer = () => {
             </div>
           </Link>
 
-          <Link href="https://www.linkedin.com/in/mahnoor-fatima-f-76782b257/" passHref>
+          <Link
+            href="https://www.linkedin.com/in/mahnoor-fatima-f-76782b257/"
+            passHref
+          >
             <div className="text-white hover:text-purple-500 text-2xl hover:scale-110 hover:rotate-12 transition-all duration-300">
               <i className="fab fa-linkedin"></i>
             </div>
@@ -108,7 +45,9 @@ const Footer = () => {
             <div className="hover:underline hover:text-purple-500">About</div>
           </Link>
           <Link href="/projects" passHref>
-            <div className="hover:underline hover:text-purple-500">Projects</div>
+            <div className="hover:underline hover:text-purple-500">
+              Projects
+            </div>
           </Link>
           <Link href="/contact" passHref>
             <div className="hover:underline hover:text-purple-500">Contact</div>
@@ -124,4 +63,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
--- src/app/components/TechStack.tsx
+++ src/app/components/TechStack.tsx
@@ -1,57 +1,3 @@
-// "use client";
-// import React, { useEffect } from "react";
-// import AOS from "aos";
-// import "aos/dist/aos.css";
-// import {
-//   FaHtml5,
-//   FaCss3Alt,
-//   FaJsSquare,
-//   FaReact,
-//   FaGitAlt,
-//   FaGithub,
-// } from "react-icons/fa";
-// import {
-//   SiTailwindcss,
-//   SiVisualstudiocode,
-//   SiTypescript,
-//   SiNextdotjs,
-// } from "react-icons/si";
-
-// const TechStack: React.FC = () => {
-//   useEffect(() => {
-//     AOS.init();
-//   }, []);
-
-//   return (
-//     <section
-//       data-aos="fade-up"
-//       className="text-center mt-20 max-w-screen-xl mx-auto p-6 md:p-8 min-h-[500px] mb-20 bg-white font-serif"
-//     >
-//       <h2 className="text-3xl sm:text-4xl font-bold text-gray-800">
-//         My Tech Stack
-//       </h2>
-//       <p className="text-lg sm:text-2xl text-gray-600 mt-2">
-//         Technologies I&#8217;ve been working with recently
-//       </p>
-
-//       <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-6 sm:gap-8 mt-8 justify-items-center">
-//         <FaHtml5 size={68} className="text-orange-500" />
-//         <FaCss3Alt size={68} className="text-blue-600" />
-//         <FaJsSquare size={68} className="text-yellow-500" />
-//         <FaReact size={68} className="text-blue-400" />
-//         <SiNextdotjs size={68} className="text-black" />
-//         <SiTailwindcss size={68} className="text-teal-400" />
-//         <SiTypescript size={68} className="text-blue-600" />
-//         <FaGitAlt size={68} className="text-red-500" />
-//         <FaGithub size={68} className="text-gray-800" />
-//         <SiVisualstudiocode size={68} className="text-blue-500" />
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default TechStack;
-
 "use client";
 import React, { useEffect } from "react";
 import AOS from "aos";
@@ -154,13 +100,11 @@ const TechStack: React.FC = () => {
         <div
           className="hover:bg-yellow-200 hover:scale-110 transition-all duration-300 ease-in-out p-3 rounded-full"
           data-aos="fade-up"
-          
         >
           <Image
             src="" // Replace with your Python icon path
             alt="Python"
             className="h-[95px] w-[95px]"
-
           />
         </div>
       </div>
@@ -169,5 +113,3 @@ const TechStack: React.FC = () => {
 };
 
 export default TechStack;
-
-
