e3f0db9f97ed403a71bafb17a4770cef58150c38
Mahnoor Fatima Faizi <mahnoorf2002@gmail.com>
2024-11-19 00:38:59 +0500

    updates

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,90 +1,7 @@
-// import Image from "next/image";
-// import Link from "next/link";
-
-// const Hero = () => {
-//   return (
-//     <section className="max-w-screen-xl mx-auto px-6 py-12 lg:flex lg:justify-between lg:items-center font-serif">
-//       <div className="z-10 space-y-5 text-center lg:text-left lg:w-1/2">
-//         <div className="flex flex-wrap justify-center lg:justify-start items-center space-x-2">
-//           <h1 className="text-3xl sm:text-4xl lg:text-5xl font-semibold text-customAmber">
-//             Hi{" "}
-//             <span role="img" aria-label="wave">
-//               👋
-//             </span>
-//             ,
-//           </h1>
-//         </div>
-//         <div className="block">
-//           <h2 className="text-2xl sm:text-4xl lg:text-5xl font-semibold text-customWhite ">
-//             I am{" "}
-//             <p className="text-customEarthyOrange text-typing">
-//               Mahnoor Fatima Faizi{" "}
-//             </p>
-//           </h2>
-//         </div>
-//         <p className="text-xl sm:text-2xl lg:text-3xl text-customTurquoise mt-2">
-//           Passionate Web Developer
-//         </p>
-
-//         <div className="flex justify-center lg:justify-start space-x-6 mt-6">
-//           {/* <a
-//             href="/about"
-//             className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg transition-all duration-300 ease-in-out"
-//           >
-//             About
-//           </a>
-//           <a
-//             href="/contact"
-//             className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg transition-all duration-300 ease-in-out"
-//           >
-//             Contact
-//           </a> */}
-//           <a
-//   href="/about"
-//   className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg hover:scale-105 hover:ring-2 hover:ring-[#32e1e6] active:scale-95 transition-transform duration-300 ease-in-out"
-// >
-//   About
-// </a>
-
-// <a
-//   className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg hover:scale-105 hover:ring-2 hover:ring-[#32e1e6] active:scale-95 transition-transform duration-300 ease-in-out"
-// >
-// <Link
-//           href="https://www.linkedin.com/in/mahnoor-fatima-f-76782b257/"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//         Linkedin
-//         </Link>
-// </a>
-
-//         </div>
-//       </div>
-
-//       <div className="lg:w-1/2 flex justify-center lg:justify-end mt-8 lg:mt-0">
-//         <Image
-//           src="/images/portfolioimage.png"
-//           alt="Portfolio Image"
-//           width={300}
-//           height={300}
-//           className="rounded-full border-4 border-customYellowOrange"
-//           priority
-//         />
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Hero;
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
 
 const Hero = () => {
-  useEffect(() => {
-    // Optional: Initialize any other animation libraries or settings if needed
-  }, []);
-
   return (
     <section className="max-w-screen-xl mx-auto px-6 py-12 lg:flex lg:justify-between lg:items-center font-serif">
       <div className="z-10 space-y-5 text-center lg:text-left lg:w-1/2">
@@ -105,29 +22,42 @@ const Hero = () => {
             </p>
           </h2>
         </div>
-        <p className="text-xl sm:text-2xl lg:text-3xl text-customTurquoise mt-2 opacity-0 animate-fadeIn">
+        <p className="text-xl sm:text-2xl lg:text-3xl text-customTurquoise mt-2">
           Passionate Web Developer
         </p>
 
         <div className="flex justify-center lg:justify-start space-x-6 mt-6">
-          <a
+          {/* <a
             href="/about"
-            className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg hover:scale-105 hover:ring-2 hover:ring-[#32e1e6] active:scale-95 transition-transform duration-300 ease-in-out"
+            className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg transition-all duration-300 ease-in-out"
           >
             About
           </a>
-
           <a
-            className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg hover:scale-105 hover:ring-2 hover:ring-[#32e1e6] active:scale-95 transition-transform duration-300 ease-in-out"
+            href="/contact"
+            className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg transition-all duration-300 ease-in-out"
           >
-            <Link
-              href="https://www.linkedin.com/in/mahnoor-fatima-f-76782b257/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Linkedin
-            </Link>
-          </a>
+            Contact
+          </a> */}
+          <a
+  href="/about"
+  className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg hover:scale-105 hover:ring-2 hover:ring-[#32e1e6] active:scale-95 transition-transform duration-300 ease-in-out"
+>
+  About
+</a>
+
+<a
+  className="px-6 py-3 bg-[#065a60] text-white text-lg font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg hover:scale-105 hover:ring-2 hover:ring-[#32e1e6] active:scale-95 transition-transform duration-300 ease-in-out"
+>
+<Link
+          href="https://www.linkedin.com/in/mahnoor-fatima-f-76782b257/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Linkedin
+        </Link>
+</a>
+
         </div>
       </div>
 
@@ -146,3 +76,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,137 +1,6 @@
-// "use client";
-
-// import React, { useEffect, useState } from "react";
-
-// interface ProjectProps {
-//   image: string;
-//   title: string;
-//   description: string;
-//   techStack: string[];
-//   githubLink: string;
-//   vercelLink: string;
-// }
-
-// const ProjectCard: React.FC<ProjectProps> = ({
-//   image,
-//   title,
-//   description,
-//   techStack,
-//   githubLink,
-//   vercelLink,
-// }) => {
-//   return (
-//     <div className="flex flex-col md:flex-row items-center justify-between bg-white shadow-lg rounded-lg p-6 space-x-6 bg-opacity-90 ">
-//       <div className="flex-shrink-0 w-full md:w-1/3 mb-6 md:mb-0">
-//         <img
-//           src={image}
-//           alt={title}
-//           className="w-full h-[250px] object-cover rounded-lg"
-//         />
-//       </div>
-
-//       <div className="w-full md:w-2/3 font-serif">
-//         <h3 className="text-2xl font-semibold text-gray-900 mb-4">{title}</h3>
-//         <p className="text-gray-600 mb-4">{description}</p>
-
-//         <div className="flex flex-wrap gap-2 mb-4">
-//           {techStack.map((tech, index) => (
-//             <span
-//               key={index}
-//               className="bg-teal-200 text-teal-800 py-1 px-3 rounded-full text-sm"
-//             >
-//               {tech}
-//             </span>
-//           ))}
-//         </div>
-
-//         <div className="flex space-x-4">
-//           <a
-//             href={githubLink}
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className="text-teal-500 font-medium hover:underline"
-//           >
-//             GitHub
-//           </a>
-//           <a
-//             href={vercelLink}
-//             target="_blank"
-//             rel="noopener noreferrer"
-//             className="text-teal-500 font-medium hover:underline"
-//           >
-//             Preview
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// const Projects = () => {
-//   const [isClient, setIsClient] = useState(false);
-
-//   useEffect(() => {
-//     setIsClient(true);
-//   }, []);
-
-//   if (!isClient) {
-//     return null;
-//   }
-
-//   return (
-//     <section className="py-16 bg-background text-foreground relative font-serif">
-//       <div className="absolute inset-0 bg-white bg-opacity-20"></div>
-
-//       <div className="max-w-7xl mx-auto px-6 relative z-10">
-//         <h2 className="text-3xl font-bold text-center mb-12 text-white">
-//           My Projects
-//         </h2>
-
-//         <div className="space-y-12">
-//           <ProjectCard
-//             image="/images/recipe_website.png"
-//             title="Project: Cookieen (Recipe Website)"
-//             description="Cookieen is a recipe website built with Next.js, TypeScript, and Tailwind CSS. It features responsive design and shadcn UI components. The project showcases my skills in creating user-friendly, modern websites."
-//             techStack={[
-//               "Next.js",
-//               "React",
-//               "Tailwind CSS",
-//               "TypeScript",
-//               "Shadcn UI",
-//             ]}
-//             githubLink="https://github.com/mahnoorfatimafaizi/Milestone2-Basic-UI-Design-using-Tailwind-CSS.git"
-//             vercelLink="https://alm4-cookieen.vercel.app/"
-//           />
-
-//           <ProjectCard
-//             image="/images/ai&finance.png"
-//             title="Project: AI & Finance"
-//             description="AI & Finance is a sample project built with Next.js, TypeScript, and custom CSS. It features a responsive design and pixel-perfect layouts. This project showcases my skills in creating modern, user-friendly websites."
-//             techStack={["Next.js", "React", "Custom CSS"]}
-//             githubLink="https://github.com/mahnoorfatimafaizi/Milestone2-Basic-UI-Design-with-Custom-CSS.git"
-//             vercelLink="https://aiandfinance.vercel.app/"
-//           />
-//         </div>
-
-//         <div className="flex justify-center mt-6">
-//           <a
-//             href="/projects"
-//             className="px-6 py-3 bg-[#065a60] text-white text-bold font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg transition-all duration-300"
-//           >
-//             See more projects
-//           </a>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Projects;
-
 "use client";
 
-import React, { useState } from "react";
-import { useInView } from "react-intersection-observer";
+import React, { useEffect, useState } from "react";
 
 interface ProjectProps {
   image: string;
@@ -150,13 +19,8 @@ const ProjectCard: React.FC<ProjectProps> = ({
   githubLink,
   vercelLink,
 }) => {
-  const [showDetails, setShowDetails] = useState(false);
-
   return (
-    <div
-      className="flex flex-col md:flex-row items-center justify-between bg-white shadow-lg rounded-lg p-6 space-x-6 bg-opacity-90 
-        hover:shadow-2xl hover:scale-105 transition-transform duration-300"
-    >
+    <div className="flex flex-col md:flex-row items-center justify-between bg-white shadow-lg rounded-lg p-6 space-x-6 bg-opacity-90 ">
       <div className="flex-shrink-0 w-full md:w-1/3 mb-6 md:mb-0">
         <img
           src={image}
@@ -180,13 +44,7 @@ const ProjectCard: React.FC<ProjectProps> = ({
           ))}
         </div>
 
-        {showDetails && (
-          <div className="text-gray-500 text-sm mb-4">
-            Tools used: {techStack.join(", ")}
-          </div>
-        )}
-
-        <div className="flex space-x-4 items-center">
+        <div className="flex space-x-4">
           <a
             href={githubLink}
             target="_blank"
@@ -203,12 +61,6 @@ const ProjectCard: React.FC<ProjectProps> = ({
           >
             Preview
           </a>
-          <button
-            onClick={() => setShowDetails(!showDetails)}
-            className="text-teal-500 font-medium hover:underline focus:outline-none"
-          >
-            {showDetails ? "Hide Details" : "Read More"}
-          </button>
         </div>
       </div>
     </div>
@@ -216,30 +68,26 @@ const ProjectCard: React.FC<ProjectProps> = ({
 };
 
 const Projects = () => {
-  const { ref, inView } = useInView({
-    threshold: 0.2, // Trigger when 20% of the element is visible
-    triggerOnce: true, // Only trigger the animation once
-  });
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
 
   return (
     <section className="py-16 bg-background text-foreground relative font-serif">
       <div className="absolute inset-0 bg-white bg-opacity-20"></div>
 
       <div className="max-w-7xl mx-auto px-6 relative z-10">
-        <h2
-          className={`text-3xl font-bold text-center mb-12 text-white transition-opacity duration-700 ease-out ${
-            inView ? "opacity-100" : "opacity-0"
-          }`}
-          ref={ref}
-        >
+        <h2 className="text-3xl font-bold text-center mb-12 text-white">
           My Projects
         </h2>
 
-        <div
-          className={`space-y-12 transition-transform duration-700 ease-out ${
-            inView ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
-          }`}
-        >
+        <div className="space-y-12">
           <ProjectCard
             image="/images/recipe_website.png"
             title="Project: Cookieen (Recipe Website)"
@@ -266,18 +114,12 @@ const Projects = () => {
         </div>
 
         <div className="flex justify-center mt-6">
-          {/* <a
+          <a
             href="/projects"
             className="px-6 py-3 bg-[#065a60] text-white text-bold font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg transition-all duration-300"
           >
             See more projects
-          </a> */}
-          <a
-  href="/projects"
-  className="px-6 py-3 bg-[#065a60] text-white text-bold font-bold rounded-md shadow-md hover:bg-gray-700 hover:shadow-lg hover:scale-105 hover:ring-2 hover:ring-[#32e1e6] active:scale-95 transition-transform duration-300 ease-in-out"
->
-  See more projects
-</a>
+          </a>
         </div>
       </div>
     </section>
@@ -285,4 +127,6 @@ const Projects = () => {
 };
 
 export default Projects;
+
+
 
